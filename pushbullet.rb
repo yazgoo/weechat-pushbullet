79061a1ea6f3d914432aa6f501b2cf080ae575da
yazgoo <yazgoo@gmail.com>
2018-11-15 08:37:39 +0100

    push, new sms

--- pushbullet.rb
+++ pushbullet.rb
@@ -13,20 +13,30 @@ def send_sms(data, command, rc, out, err)
   b = data["b"]
   input_data = data["input_data"]
   Weechat.print(b, ">\t#{input_data}")
+  Weechat.print("", "send_sms: #{[data, command, rc, out, err]}")
   if rc.to_i > 0
     Weechat.print(b, ">\tfailed sending #{input_data} (rc: #{rc})")
   end
   return Weechat::WEECHAT_RC_OK
 end
 
-def buffer_input_cb(data, b, input_data)
-  device = Weechat.buffer_get_string(b, "localvar_device")
-  input_data.force_encoding('UTF-8')
-  req = $presbeus.send_sms device, data, input_data
+def send_stuff(data, b, input_data, req)
+  Weechat.print("", "send_stuff(#{[data, b, input_data, req]})")
   args = h(req).merge({"postfields" => req[:payload].to_s, "post" => 1})
   Weechat.hook_process_hashtable(
     "url:#{req[:url]}", args, 120 * 1000, "send_sms", {b: b, input_data: input_data}.to_json)
-  return Weechat::WEECHAT_RC_OK
+  Weechat::WEECHAT_RC_OK
+end
+
+def buffer_input_cb(data, b, input_data)
+  device = Weechat.buffer_get_string(b, "localvar_device")
+  input_data.force_encoding('UTF-8')
+  send_stuff(data, b, input_data, $presbeus.send_sms(device, data, input_data))
+end
+
+def buffer_input_push(data, b, input_data)
+  input_data.force_encoding('UTF-8')
+  send_stuff(data, b, input_data, $presbeus.push(input_data))
 end
 
 def buffer_close_cb(data, buffer)
@@ -50,14 +60,19 @@ def reload_thread(data, b, args)
   return Weechat::WEECHAT_RC_OK
 end
 
+def setup_thread(device, address, name)
+  b = Weechat.buffer_new(name, 'buffer_input_cb', name, 'buffer_close_cb', name)
+  $buffers[address] = b
+  Weechat.buffer_set(b, "localvar_set_address", address)
+  Weechat.buffer_set(b, "localvar_set_device", device)
+  reload_thread(nil, b, nil)
+end
+
 def load_threads(device, command, rc, out, err)
   Weechat.print('', "loading device #{device}")
   JSON.parse(out)["threads"].map{|x| Presbeus.parse_thread(x)}.each do |address, name|
-    b = Weechat.buffer_new(name, 'buffer_input_cb', name, 'buffer_close_cb', name)
-    $buffers[address] = b
-    Weechat.buffer_set(b, "localvar_set_address", address)
-    Weechat.buffer_set(b, "localvar_set_device", device)
-    reload_thread(nil, b, nil)
+  Weechat.print('', "loading device #{device}, #{address}, #{name}")
+    setup_thread(device, address, name)
   end
   return Weechat::WEECHAT_RC_OK
 end
@@ -65,6 +80,13 @@ end
 def get_devices(data, command, rc, out, err)
   Weechat.print('', "devices:")
   JSON.parse(out)["devices"].each { |d|  Weechat.print('', "#{d["iden"]} : #{d["model"]}") }
+  return Weechat::WEECHAT_RC_OK
+end
+
+def get_pushes(data, command, rc, out, err)
+  JSON.parse(out)["pushes"].reverse.map do |push|
+    Weechat.print($buffers["pushes"], push["body"])
+  end
   return Weechat::WEECHAT_RC_OK
 end
 
@@ -87,8 +109,16 @@ def realtime(data, command, rc, out, err)
       if payload["type"] == "push"
         notifications = payload["push"]["notifications"]
         notifications.each do |push|
-          Weechat.print("", "print #{$buffers[push["thread_id"]]}, #{push["body"]}")
-          Weechat.print($buffers[push["thread_id"]], ">\t#{push["body"]}")
+          Weechat.print("", "print #{$buffers[push["thread_id"]]}, #{push}")
+          buffer = $buffers[push["thread_id"]]
+          if buffer.nil?
+            # handle new thread
+            id = push["thread_id"]
+            # todo find user name
+            setup_thread(payload["push"]["source_device_iden"], id, "unknown ##{id}")
+          else
+            Weechat.print(buffer, ">\t#{push["body"]}")
+          end
         end if notifications
       end
     rescue JSON::ParserError => e
@@ -115,6 +145,10 @@ def weechat_init
   req = $presbeus.get_v2("devices")
   Weechat.hook_process_hashtable(
     "url:#{req[:url]}", h(req), 120 * 1000, "get_devices", "")
+  $buffers["pushes"] = Weechat.buffer_new("pushes", 'buffer_input_push', "pushes", 'buffer_close_cb', "pushes") 
+  req = $presbeus.get_v2("pushes")
+  Weechat.hook_process_hashtable(
+    "url:#{req[:url]}", h(req), 120 * 1000, "get_pushes", "")
   Weechat.print('', "launch '/pb_d <device_id>' to load device")
   if !$presbeus.default_device.nil?
     load_device(nil, nil, $presbeus.default_device)
